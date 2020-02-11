12af116f77cbe811bff67540ea79693e15634868
KupiyanovVladislav <9412252@mail.ru>
2020-02-11 22:36:22 +0300

    add models to admin view

--- Main/admin.py
+++ Main/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Movie, Link, Tag, Rating
 
-# Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    pass
+
+
+class LinkAdmin(admin.ModelAdmin):
+    pass
+
+
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+class RatingAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Link, LinkAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Rating, RatingAdmin)
