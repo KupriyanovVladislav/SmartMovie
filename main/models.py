ca59a3c913bfaf1c2855811f87d5f508421bf2a5
KupiyanovVladislav <9412252@mail.ru>
2020-02-21 10:39:22 +0300

    add Meta to models

--- main/models.py
+++ main/models.py
@@ -4,6 +4,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Movie(models.Model):
+    class Meta:
+        ordering = ['movie_id']
+
     movie_id = models.PositiveIntegerField(primary_key=True)
     title = models.CharField(max_length=255)
     year = models.PositiveIntegerField(null=True, blank=True)
@@ -11,12 +14,18 @@ class Movie(models.Model):
 
 
 class Link(models.Model):
+    class Meta:
+        ordering = ['movie_id']
+
     movie_id = models.OneToOneField(Movie, on_delete=models.CASCADE, primary_key=True)
     imdb_id = models.PositiveIntegerField(null=True, blank=True)
     tmdb_id = models.PositiveIntegerField(null=True, blank=True)
 
 
 class Rating(models.Model):
+    class Meta:
+        ordering = ['user_id', 'movie_id']
+
     user_id = models.PositiveIntegerField()
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
     rating = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(5.0)])
@@ -24,6 +33,9 @@ class Rating(models.Model):
 
 
 class Tag(models.Model):
+    class Meta:
+        ordering = ['user_id', 'movie_id']
+
     user_id = models.PositiveIntegerField()
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
     tag = models.CharField(max_length=127)
